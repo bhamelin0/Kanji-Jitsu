a56bb33cadddc763057ebd79a67352b8f5034953
bhamelin0 <bhamelin@uwo.ca>
2024-09-28 18:38:01 -0400

    Basic react layout

---
+++ kanjijitsu/src/about.js
@@ -0,0 +1,17 @@
+import './kanji-game.css';
+import * as React from "react";
+
+function AboutPage() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            About Page
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default AboutPage;
+  
---
+++ kanjijitsu/src/kanji-game.js
@@ -0,0 +1,17 @@
+import './kanji-game.css';
+import * as React from "react";
+
+function KanjiGame() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Kanji Jitsu!
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default KanjiGame;
+  
